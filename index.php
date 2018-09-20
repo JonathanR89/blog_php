ef6882cc08f62fb3d841ada1ca05dfafaf8abb30
Jonathan <jonathanrautenbach@gmail.com>
2018-09-20 11:57:14 +0200

    Character limit in index page

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                 <hr>
                 <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
-                <p><?php echo $post_content ?></p>
+                <p><?php echo substr($post_content, 0, 500) ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
