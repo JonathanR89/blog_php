c11bba17110ce7e34dfb00c13cd9ecdce75886b1
Jonathan <jonathanrautenbach@gmail.com>
2018-10-01 15:28:45 +0200

    Add post link to image

--- index.php
+++ index.php
@@ -36,11 +36,11 @@
                     <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
+                    by <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>"><img class="img-responsive" src="images/<?php echo $post_image ?>" alt=""></a>
                 <hr>
                 <p><?php echo substr($post_content, 0, 500) ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
